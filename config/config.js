83a5b26df5e0107974fd6c16aeb98308cf794829
Hurricane.TN <houssem.rouabeh@toucomex.com>
2025-05-22 13:25:05 +0100

    test deploy 3

--- config/config.js
+++ config/config.js
@@ -20,7 +20,7 @@ export default {
     },
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    use_env_variable: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
